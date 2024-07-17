c675d3d69af49257bff0e621aa6930e76ea8f59b
RopeJuve <rope.s@hotmail.com>
2024-07-17 19:52:42 +0200

    Initial Commit

--- crudOperations.js
+++ crudOperations.js
@@ -1,46 +1,93 @@
 // Import utility functions
-import { getResourceId, processBodyFromRequest, returnErrorWithMessage } from './utils.js';
+import {
+  getResourceId,
+  processBodyFromRequest,
+  returnErrorWithMessage,
+} from "./utils.js";
+import { pool } from "./db.js";
 
 export const createPost = async (req, res) => {
   try {
     const body = await processBodyFromRequest(req);
-    if (!body) return returnErrorWithMessage(res, 400, 'Body is required');
+    if (!body) return returnErrorWithMessage(res, 400, "Body is required");
     const parsedBody = JSON.parse(body);
-    console.log('Here we have access to the body: ', parsedBody);
+    console.log("Here we have access to the body: ", parsedBody);
+    const result = await pool.query(
+      "INSERT INTO posts (title,author, content) VALUES ($1, $2, $3) RETURNING *",
+      [parsedBody.title, parsedBody.author, parsedBody.content]
+    );
+
     res.statusCode = 201;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ message: 'Post created' }));
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(result.rows[0]));
   } catch (error) {
     returnErrorWithMessage(res);
   }
 };
 
-export const getPosts = (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ message: 'Posts fetched' }));
+export const getPosts = async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM posts");
+    console.log("Here we have access to the result: ", result);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(result.rows));
+  } catch (error) {
+    returnErrorWithMessage(res);
+  }
 };
 
-export const getPostById = (req, res) => {
-  const id = getResourceId(req.url);
-  console.log('Here we have access to the ID: ', id);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ message: 'Post fetched' }));
+export const getPostById = async (req, res) => {
+  try {
+    const id = getResourceId(req.url);
+    console.log("Here we have access to the ID: ", id);
+    const result = await pool.query("SELECT * FROM posts WHERE id = $1", [id]);
+    if (result.rows.length === 0)
+      return returnErrorWithMessage(res, 404, "Post not found");
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(result.rows[0]));
+  } catch (error) {
+    returnErrorWithMessage(res);
+  }
 };
 
-export const updatePost = (req, res) => {
-  const id = getResourceId(req.url);
-  console.log('Here we have access to the ID: ', id);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ message: 'Post updated' }));
+export const updatePost = async (req, res) => {
+  try {
+    const id = getResourceId(req.url);
+    console.log("Here we have access to the ID: ", id);
+    const body = await processBodyFromRequest(req);
+    if (!body) return returnErrorWithMessage(res, 400, "Body is required");
+    const parsedBody = JSON.parse(body);
+    console.log("Here we have access to the body: ", parsedBody);
+    const result = await pool.query(
+      "UPDATE posts SET title = $1, author = $2, content = $3 WHERE id = $4 RETURNING *",
+      [parsedBody.title, parsedBody.author, parsedBody.content, id]
+    );
+    if (result.rows.length === 0)
+      return returnErrorWithMessage(res, 404, "Post not found");
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(result.rows[0]));
+  } catch (error) {
+    returnErrorWithMessage(res);
+  }
 };
 
-export const deletePost = (req, res) => {
-  const id = getResourceId(req.url);
-  console.log('Here we have access to the ID: ', id);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ message: 'Post deleted' }));
+export const deletePost = async (req, res) => {
+  try {
+    const id = getResourceId(req.url);
+    console.log("Here we have access to the ID: ", id);
+    const postForDeleting = await pool.query(
+      "DELETE FROM posts WHERE id = $1",
+      [id]
+    );
+    if (postForDeleting.rowCount === 0)
+      return returnErrorWithMessage(res, 404, "Post not found");
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify({ message: "Post deleted" }));
+  } catch (error) {
+    returnErrorWithMessage(res);
+  }
 };
--- index.js
+++ index.js
@@ -1,32 +1,42 @@
 // Bring in the http module
-import http from 'http';
+import http from "http";
+import dotenv from "dotenv";
+dotenv.config();
 // Import CRUD operations
-import { createPost, deletePost, getPosts, getPostById, updatePost } from './crudOperations.js';
+import {
+  createPost,
+  deletePost,
+  getPosts,
+  getPostById,
+  updatePost,
+} from "./crudOperations.js";
 // Import utility functions
-import { regex, returnErrorWithMessage } from './utils.js';
+import { regex, returnErrorWithMessage } from "./utils.js";
 
 // Base resource
-const resource = '/posts';
+const resource = "/posts";
 
 // Request handler to handle all requests
 const requestHandler = async (req, res) => {
   const { method, url } = req;
   if (url === resource) {
-    if (method === 'GET') return await getPosts(req, res);
-    if (method === 'POST') return await createPost(req, res);
-    else return returnErrorWithMessage(res, 405, 'Method Not Allowed');
+    if (method === "GET") return await getPosts(req, res);
+    if (method === "POST") return await createPost(req, res);
+    else return returnErrorWithMessage(res, 405, "Method Not Allowed");
   } else if (regex(resource).test(url)) {
-    if (method === 'GET') return await getPostById(req, res);
-    if (method === 'PUT') return await updatePost(req, res);
-    if (method === 'DELETE') return await deletePost(req, res);
-    else return returnErrorWithMessage(res, 405, 'Method Not Allowed');
+    if (method === "GET") return await getPostById(req, res);
+    if (method === "PUT") return await updatePost(req, res);
+    if (method === "DELETE") return await deletePost(req, res);
+    else return returnErrorWithMessage(res, 405, "Method Not Allowed");
   } else {
-    return returnErrorWithMessage(res, 404, 'Resource Not Found');
+    return returnErrorWithMessage(res, 404, "Resource Not Found");
   }
 };
 // Create a server
 const server = http.createServer(requestHandler);
 // Set the port
-const port = 3000;
+const port = process.env.PORT || 5000;
 // Start the server
-server.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+server.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
